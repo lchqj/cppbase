3a4865cddbcc0c1bca30cfdeba43e5f61923f528
ginkgo <297081297@qq.com>
2017-12-24 17:17:50 +0800

    Update tcpserver.h

---
+++ tcpserver/tcpserver.h
@@ -0,0 +1,20 @@
+#ifndef cppbase_tcp_server_h
+#define cppbase_tcp_server_h
+
+
+#include <string>
+#include <vector>
+#include <boost/asio.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/noncopyable.hpp>
+
+using namespace std;
+
+namespace cppbase{
+
+class tcp_server: private boost::noncopyable{
+};
+}
+
+
+#endif
