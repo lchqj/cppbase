c38de17cb97ed8666798147fb8d6a7e598984c6d
lihuijun <297081297@qq.com>
2017-12-24 20:54:56 +0800

    格式化

--- tcpserver/tcpserver.h
+++ tcpserver/tcpserver.h
@@ -10,11 +10,26 @@
 
 using namespace std;
 
-namespace cppbase{
+namespace cppbase
+{
 
-class tcp_server: private boost::noncopyable{
+class tcp_server : private boost::noncopyable
+{
+    /*
+    * 连接监视器，监视连接到达和连接超时
+    */
+    class conn_monitor
+    {
+    };
+
+    /*
+    * 会话池
+    */
+    class session_pool
+    {
+    };
+    
 };
 }
-
 
 #endif
